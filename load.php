7976cfb9689681f49fa323386d2dc81810c26ff5
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-08-16 09:53:37 -0300

    Adding EncryptedButton.php to load.php

--- load.php
+++ load.php
@@ -1,6 +1,7 @@
 <?php
 
 // Just include this file if you don't have an autoloader
+require 'src/PayPal/EncryptedButton.php';
 require 'src/PayPal/IpnRequest.php';
 require 'src/PayPal/PdtRequest.php';
 require 'src/PayPal/SecurityException.php';
